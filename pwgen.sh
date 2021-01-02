607a45e871ee1f9bf57551d00880058bc328913a
mrm3ss <43171770+mrm3ss@users.noreply.github.com>
2021-01-02 15:47:15 +0100

    Create pwgen.sh

---
+++ pwgen.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+##===============================================================
+##
+## USAGE: 
+##    pwgen.sh password
+##
+## DESCRIPTION:
+##    Hisilicon 3516 based IPC md5 password hash generation
+##   
+## IMPLEMENTATION
+##    version         1.0.0.1
+##    author          Ivan Stimac
+##    copyright       Copyright (c) http://www.invizum.com
+##    license         GNU General Public License
+##
+##===============================================================
+
+
+if [ $# -gt 0 ] && ( [ $1 == "-h" ] || [ $1 == "--help" ] ); then
+
+	printf "Usage: pwgen.sh password\n"
+	exit
+
+fi
+
+
+
+pwd=$1
+
+pwdMd5=$(echo -n $pwd | md5sum | awk '{print $1}')
+
+ipcHash=""
+
+
+
+declare -i byteL
+declare -i byteH
+
+for  (( i =0; i<8; i++ ))
+do
+
+	byteL=0x${pwdMd5:i*4:2}
+	byteH=0x${pwdMd5:(i*4)+2:2}
+
+	#printf "%02x %02x\n" $byteH $byteL
+	
+	declare -i x=$(($byteL+$byteH))
+	declare -i xMod=$(($x%0x3e))
+	
+	#printf "%02x\n" $xMod
+	
+	if (($xMod < 0x09)); then 
+		xMod=$(($xMod + 0x30))
+	elif (($xMod >= 0x09 && $xMod < 0x0a)); then
+		xMod=$(($xMod + 0x3d))
+	elif (($xMod >= 0x0a && $xMod < 0x23)); then
+		xMod=$(($xMod + 0x37))
+	elif (($xMod >= 0x0a && $xMod >= 0x23)); then
+		xMod=$(($xMod + 0x3d))
+	fi
+	
+
+	#printf "%02x " $xMod;
+	ipcHash+=$(printf "\x$(printf %x $xMod)")
+done
+
+
+# echo the result
+printf "Plain password: %s\n" $pwd
+printf "Md5Sum: %s\n" $pwdMd5
+printf "Encripted ipc hash: %s\n" $ipcHash
+
+
